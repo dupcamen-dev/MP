bd22deeb567681c26f35fcca5550a4fb43dd4054
dupcamen-dev <dupcamen-dev@github.com>
2026-07-17 20:35:51 +0300

    Fix manifesto: all brown text same sienna color

--- src/components/ManifestoSlide.jsx
+++ src/components/ManifestoSlide.jsx
@@ -26,7 +26,7 @@ export default function ManifestoSlide({ progress }) {
         lineHeight: 0.95, textTransform: 'uppercase',
         color: 'var(--cream)', margin: 0, letterSpacing: '0.005em',
       }}>
-        <span style={{ color: 'var(--terracotta)' }}>We don&apos;t do meetings about meetings.</span><br />
+        <span style={{ color: 'var(--sienna)' }}>We don&apos;t do meetings about meetings.</span><br />
         <span style={{ color: 'var(--sienna)' }}>We do live </span><span style={{ color: 'var(--cream)' }}>software</span>,<br />
         <span style={{ color: 'var(--sienna)' }}>in </span><span style={{ color: 'var(--cream)' }}>seven days</span>.
       </h2>
